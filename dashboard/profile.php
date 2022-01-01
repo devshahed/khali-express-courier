39f6f1611d1cfc70854d6ea596be5c9b34fa4867
root <root@lazyCoder.shahed>
2022-01-01 16:46:54 +0600

    dashboard profile

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/all.min.css">
-    <link rel="stylesheet" href="css/global.css">
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/style.css" type="text/css" />
     <link rel="icon" href="images/favicon.ico">
 </head>
 
@@ -16,16 +16,103 @@
     <?php include_once 'header.php'; ?>
     <main>
         <!-- ================= Profile ================ -->
-        <section id="register" class="background-no-repeat background-cover background-fixed background-center">
+        <section id="profile" class="bg-danger text-white">
             <div class="container">
-                
+                <div class="row">
+                    <div class="col-8">
+                        <form action="?" method="POST">
+                            <div class="mb-3">
+                                <label class="mb-1">Mailbox #</label>
+                                <input class="form-control bg-transparent border border-3 border-white text-white" type="text" value="226" disabled readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="fname" class="mb-1">First Name</label>
+                                <input id="fname" class="form-control bg-transparent border border-3 border-white text-white" type="text" name="fname" value="Simone">
+                            </div>
+                            <div class="mb-3">
+                                <label for="lname" class="mb-1">Last Name</label>
+                                <input id="lname" class="form-control bg-transparent border border-3 border-white text-white" type="text" name="lname" value="Reid">
+                            </div>
+                            <div class="mb-3">
+                                <label for="phone" class="mb-1">Phone</label>
+                                <input id="phone" class="form-control bg-transparent border border-3 border-white text-white" type="text" name="Phone" value="826293****">
+                            </div>
+                            <div class="mb-3">
+                                <label for="trn" class="mb-1">TRN</label>
+                                <input id="trn" class="form-control bg-transparent border border-3 border-white text-white" type="text" name="trn" value="122121678">
+                            </div>
+                            <h5 class="mb-3">Primary Address</h5>
+                            <div class="mb-3">
+                                <label for="addr" class="mb-1">Address</label>
+                                <input id="addr" class="form-control bg-transparent border border-3 border-white text-white" type="text" name="address" value="603 Walkway 23, Braeton  Newtown">
+                            </div>
+                            <div class="mb-3">
+                                <label for="city" class="mb-1">City</label>
+                                <input id="city" class="form-control bg-transparent border border-3 border-white text-white" type="text" name="city" value="Greater Portmore">
+                            </div>
+                            <div class="mb-3">
+                                <label for="parish" class="mb-3">Parish</label>
+                                <select name="parish" id="parish">
+                                    <option disabled>Select Parish</option>
+                                    <option selected value="St.Catherine">St.Catherine</option>
+                                    <option value="St.Catherine">St.Catherine</option>
+                                    <option value="St.Catherine">St.Catherine</option>
+                                    <option value="St.Catherine">St.Catherine</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="mb-1">Email</label>
+                                <input id="email" class="form-control bg-transparent border border-3 border-white text-white" type="email" name="email" value="Greater Portmore">
+                            </div>
+                        </form>
+                        <form action="?" method="POST">
+                            <h4>Password Management</h4>
+                            <div class="mb-3">
+                                <label for="oldPass" class="mb-1">Old Password</label>
+                                <input id="oldPass" class="form-control bg-transparent border border-3 border-white text-white" type="password" name="old-pass">
+                            </div>
+                            <div class="mb-3">
+                                <label for="newPass" class="mb-1">New Password</label>
+                                <input id="newPass" class="form-control bg-transparent border border-3 border-white text-white" type="password" name="new-pass">
+                            </div>
+                            <div class="mb-3">
+                                <label for="confirmPass" class="mb-1">Old Password</label>
+                                <input id="confirmPass" class="form-control bg-transparent border border-3 border-white text-white" type="password" name="confirm-pass">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-4 d-flex flex-column">
+                        <div class="p-3 rounded-2 my-3 bg-primary">
+                            <p>Member Since (2020-06-28)</p>
+                            <div class="d-flex justify-content-between align-tems-center">
+                                <i class="fas fa-calendar-alt fa-2x d-block"></i>
+                                <p class="mb-0">1 year ago</p>
+                            </div>
+                        </div>
+                        <div class="p-3 rounded-2 my-3 bg-primary">
+                            <p>Total Items Shipped</p>
+                            <div class="d-flex justify-content-between align-tems-center">
+                                <i class="fas fa-box fa-2x d-block"></i>
+                                <p class="mb-0">0</p>
+                            </div>
+                        </div>
+                        <div class="p-3 rounded-2 my-3 bg-primary">
+                            <p>Authorize Pickup</p>
+                            <div class="d-flex justify-content-between align-tems-center">
+                                <i class="fas fa-lock-open fa-2x d-block"></i>
+                                <p class="mb-0">0</p>
+                            </div>
+                            <p class="mb-0 mt-2"><a href="#" class="text-white text-decoration-none">Add/Update</a></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
     <?php include_once 'footer.php'; ?>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/all.min.js"></script>
-    <script src="js/app.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/all.min.js"></script>
+    <script src="../js/app.js"></script>
 </body>
 
 </html>
