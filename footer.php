2e9f01fa37c5d1a3eac191e24e592dea7b97e72d
Shahed <shahed@lazyCoder.shahed>
2022-01-04 14:22:48 +0600

    f

--- footer.php
+++ footer.php
@@ -1,1 +1,35 @@
-<?php echo "footer"; ?>
+<footer class="pt-4 text-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-12">
+                <div class="px-5">
+                    <img src="../images/logotransparent.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-6 col-12 pt-md-0 pt-3">
+                <h5>Contact us</h5>
+                <hr class="my-2">
+                <div class="py-1 d-flex align-items-center">
+                    <i class="fa fa-map-marker d-block me-2 text-small" aria-hidden="true"></i>
+                    <p class="mb-0 text-small text-bold">1B Marliemount Ave, Pembroke Hall, Kingston 20.</p>
+                </div>
+                <div class="py-1 d-flex align-items-center">
+                    <i class="fa fa-phone d-block me-2 text-small" aria-hidden="true"></i>
+                    <p class="mb-0 text-small text-bold">0153455435353</p>
+                </div>
+                <div class="py-1 d-flex align-items-center">
+                    <i class="fa fa-envelope d-block me-2 text-small" aria-hidden="true"></i>
+                    <p class="mb-0 text-small text-bold">info@kec.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row py-2 bg-indigo text-white text-center w-100 m-0">
+        <div class="col-md-6 col-12">
+            <p class="mb-0">Designed by <a href="https://www.facebook.com/its.devshahed" target="_blank">Devshahed</a></p>
+        </div>
+        <div class="col-md-6 col-12">
+            <p class="mb-0">&copy 2022 Khali Express Courier All rights reserved</p>
+        </div>
+    </div>
+</footer>
--- header.php
+++ header.php
@@ -1,1 +1,14 @@
-<?php echo "header"; ?>
+<header class="position-fixed top-0 left-0 w-100 bg-white text-theme-color d-flex align-items-center">
+    <div class="container-fluid d-flex align-items-center p-3">
+        <a href="/" class="me-3 d-block"><img src="/images/logotransparent.png" alt="logo" width="100" height=""></a>
+        <nav class="d-flex justify-content-between align-items-center w-100">
+            <ul class="d-flex justify-content-center align-items-center list-unstyled mb-0">
+                <li class="px-1"><a href="./" class="p-1 text-decoration-none text-theme-color">Home</a></li>
+                <li class="px-1"><a href="/about.php" class="p-1 text-decoration-none text-theme-color">About</a></li>
+                <li class="px-1"><a href="/dashboard/track.php" class="p-1 text-decoration-none text-theme-color">Services</a></li>
+                <li class="px-1"><a href="/contact.php" class="p-1 text-decoration-none text-theme-color">Contact</a></li>
+                <li class="px-1"><a href="/login.php" class="p-1 text-decoration-none text-theme-color">Login</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
